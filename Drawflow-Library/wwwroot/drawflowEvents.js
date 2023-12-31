8139c49e31604129dc6260ee9086e3ee5b1f7159
TroloveCro <davor.beg@gmail.com>
2023-12-31 16:12:27 +0100

    Added DotNet InvokeMethods

--- Drawflow-Library/wwwroot/drawflowEvents.js
+++ Drawflow-Library/wwwroot/drawflowEvents.js
@@ -2,50 +2,74 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const drawflowProxy_1 = require("./drawflowProxy");
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('nodeCreated', function (id) {
+    DotNet.invokeMethod("Drawflow-Library", "nodeCreated", id);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('nodeRemoved', function (id) {
+    DotNet.invokeMethod("Drawflow-Library", "nodeRemoved", id);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('nodeSelected', function (id) {
+    DotNet.invokeMethod("Drawflow-Library", "nodeSelected", id);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('nodeUnselected', function (IsUnselected) {
+    DotNet.invokeMethod("Drawflow-Library", "nodeUnselected", IsUnselected);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('nodeMoved', function (id) {
+    DotNet.invokeMethod("Drawflow-Library", "nodeMoved", id);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('connectionStart', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "connectionStart", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('connectionCancel', function (isCanceled) {
+    DotNet.invokeMethod("Drawflow-Library", "connectionCancel", isCanceled);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('connectionCreated', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "connectionCreated", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('connectionRemoved', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "connectionRemoved", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('connectionSelected', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "connectionSelected", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('connectionUnselected', function (IsUnselected) {
+    DotNet.invokeMethod("Drawflow-Library", "connectionUnselected", IsUnselected);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('addReroute', function (node_output_id) {
+    DotNet.invokeMethod("Drawflow-Library", "addReroute", node_output_id);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('removeReroute', function (node_output_id) {
+    DotNet.invokeMethod("Drawflow-Library", "removeReroute", node_output_id);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('moduleCreated', function (module_name) {
+    DotNet.invokeMethod("Drawflow-Library", "moduleCreated", module_name);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('moduleChanged', function (module_name) {
+    DotNet.invokeMethod("Drawflow-Library", "moduleChanged", module_name);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('moduleRemoved', function (module_name) {
+    DotNet.invokeMethod("Drawflow-Library", "moduleRemoved", module_name);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('click', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "click", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('clickEnd', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "clickEnd", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('contextmenu', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "contextmenu", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('mouseMove', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "mouseMove", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('zoom', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "zoom", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('translate', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "translate", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('import', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "import", event);
 });
 drawflowProxy_1.GlobalEditor === null || drawflowProxy_1.GlobalEditor === void 0 ? void 0 : drawflowProxy_1.GlobalEditor.on('export', function (event) {
+    DotNet.invokeMethod("Drawflow-Library", "export", event);
 });
--- Drawflow-Library/wwwroot/ts/drawflowEvents.ts
+++ Drawflow-Library/wwwroot/ts/drawflowEvents.ts
@@ -3,120 +3,120 @@ import { GlobalEditor as editor }  from './drawflowProxy'
 
 
 editor?.on('nodeCreated', function(id) {
-
+    DotNet.invokeMethod("Drawflow-Library", "nodeCreated", id);
 });
 
 
 editor?.on('nodeRemoved', function(id) {
-
+    DotNet.invokeMethod("Drawflow-Library", "nodeRemoved", id);
 });
 
 editor?.on('nodeSelected', function(id) {
-
+    DotNet.invokeMethod("Drawflow-Library", "nodeSelected", id);
 });
 
 
 editor?.on('nodeUnselected', function(IsUnselected) {
-
+    DotNet.invokeMethod("Drawflow-Library", "nodeUnselected", IsUnselected);
 });
 
 
 editor?.on('nodeMoved', function(id) {
-
+    DotNet.invokeMethod("Drawflow-Library", "nodeMoved", id);
 });
 
 
 editor?.on('connectionStart', function(event: ConnectionStartEvent) {
-
+    DotNet.invokeMethod("Drawflow-Library", "connectionStart", event);
 });
 
 
 editor?.on('connectionCancel', function(isCanceled) {
-
+    DotNet.invokeMethod("Drawflow-Library", "connectionCancel", isCanceled);
 });
 
 
 editor?.on('connectionCreated', function(event: ConnectionEvent) {
-
+    DotNet.invokeMethod("Drawflow-Library", "connectionCreated", event);
 });
 
 
 
 editor?.on('connectionRemoved', function(event: ConnectionEvent) {
-
+    DotNet.invokeMethod("Drawflow-Library", "connectionRemoved", event);
 });
 
 
 
 editor?.on('connectionSelected', function(event: ConnectionEvent) {
-
+    DotNet.invokeMethod("Drawflow-Library", "connectionSelected", event);
 });
 
 
 
 editor?.on('connectionUnselected', function(IsUnselected: boolean) {
-
+    DotNet.invokeMethod("Drawflow-Library", "connectionUnselected", IsUnselected);
 });
 
 
 
 editor?.on('addReroute', function(node_output_id: number) {
-
+    DotNet.invokeMethod("Drawflow-Library", "addReroute", node_output_id);
 });
 
 
 
 editor?.on('removeReroute', function(node_output_id: number) {
-
+    DotNet.invokeMethod("Drawflow-Library", "removeReroute", node_output_id);
 });
 
 
 
 editor?.on('moduleCreated', function(module_name: string) {
-
+    DotNet.invokeMethod("Drawflow-Library", "moduleCreated", module_name);
 });
 
 
 
 editor?.on('moduleChanged', function(module_name: string) {
-
+    DotNet.invokeMethod("Drawflow-Library", "moduleChanged", module_name);
 });
 
 
 editor?.on('moduleRemoved', function(module_name: string) {
-
+    DotNet.invokeMethod("Drawflow-Library", "moduleRemoved", module_name);
 });
 
 editor?.on('click', function(event: MouseEvent) {
-
+    DotNet.invokeMethod("Drawflow-Library", "click", event);
 });
 
 
 editor?.on('clickEnd', function(event: any) {
-
+    DotNet.invokeMethod("Drawflow-Library", "clickEnd", event);
 });
 
 editor?.on('contextmenu', function(event: any) {
-
+    DotNet.invokeMethod("Drawflow-Library", "contextmenu", event);
 });
 
 editor?.on('mouseMove', function(event: MousePositionEvent) {
-
+    DotNet.invokeMethod("Drawflow-Library", "mouseMove", event);
 });
 
 
 editor?.on('zoom', function(event: any) {
-
+    DotNet.invokeMethod("Drawflow-Library", "zoom", event);
 });
 
 editor?.on('translate', function(event: MousePositionEvent) {
-
+    DotNet.invokeMethod("Drawflow-Library", "translate", event);
 });
 
 editor?.on('import', function(event: any) {
-
+    DotNet.invokeMethod("Drawflow-Library", "import", event);
 });
 
 editor?.on('export', function(event: any) {
-
+    DotNet.invokeMethod("Drawflow-Library", "export", event);
 });
